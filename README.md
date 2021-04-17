380f03202f8879f6a317ed3fff0c27332739d210
Michal Takac <takacmichal92@gmail.com>
2021-04-17 22:09:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ArrayFire + Unreal Engine
+
+Testing the usage of ArrayFire for GPU computation and Unreal Engine 4 for real-time visualization.
---
+++ Source/UEAF/UEAFGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEAFGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEAF_API AUEAFGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
